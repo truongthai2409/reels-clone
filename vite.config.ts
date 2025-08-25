55e6683c3b9c9a3f9ce263e7272802498d5830f5
Truong Thai <tuoithanhxuan33@gmail.com>
2025-08-25 16:04:44 +0700

    fix path @

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath, URL } from 'node:url'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
